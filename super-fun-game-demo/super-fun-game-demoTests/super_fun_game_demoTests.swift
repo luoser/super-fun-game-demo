73b1fd92d3629fbb81afdf576f59cabc5aa3575a
Lisa Luo <lluo92@gmail.com>
2016-09-16 12:55:08 -0400

    init

---
+++ super-fun-game-demo/super-fun-game-demoTests/super_fun_game_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  super_fun_game_demoTests.swift
+//  super-fun-game-demoTests
+//
+//  Created by Lisa Luo on 9/16/16.
+//  Copyright © 2016 Lisa Luo. All rights reserved.
+//
+
+import XCTest
+@testable import super_fun_game_demo
+
+class super_fun_game_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
