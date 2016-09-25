7d13d4992d6d733886cc4d5ccbae609dc6814597
Lisa Luo <lluo92@gmail.com>
2016-09-25 15:00:02 +0800

    add some new game mechanics

--- super-fun-game-demo/super-fun-game-demo/ViewController.swift
+++ super-fun-game-demo/super-fun-game-demo/ViewController.swift
@@ -4,18 +4,33 @@ internal final class ViewController: UIViewController {
   private let viewModel: ViewModelType = ViewModel()
 
   @IBOutlet private weak var actionButton: UIButton!
+  @IBOutlet private weak var imageView: UIImageView!
+  @IBOutlet private weak var scoreLabel: UILabel!
 
   internal override func viewDidLoad() {
     super.viewDidLoad()
 
-    self.viewModel.outputs.doSomething
-      .observeNext { [weak self] msg in
-        self?.doSomething(msg)
+    self.imageView.image = nil
+    self.scoreLabel.text = "0"
+
+    self.viewModel.outputs.buttonTitle
+      .observeNext { [weak self] title in
+        self?.actionButton.titleLabel?.text = title
+    }
+
+    self.viewModel.outputs.image
+      .observeNext { [weak self] image in
+        self?.imageView.image = image
     }
 
     self.viewModel.outputs.hint
       .observeNext { [weak self] hint in
         self?.actionButton.accessibilityHint = hint
+    }
+
+    self.viewModel.outputs.scoreLabel
+      .observeNext { [weak self] score in
+        self?.scoreLabel.text = score
     }
   }
 
--- super-fun-game-demo/super-fun-game-demo/ViewModel.swift
+++ super-fun-game-demo/super-fun-game-demo/ViewModel.swift
@@ -3,11 +3,14 @@ import Result
 
 public protocol ViewModelInputs {
   func actionButtonTapped()
+  func viewDidLoad()
 }
 
 public protocol ViewModelOutputs {
-  var doSomething: Signal<String, NoError> { get }
+  var buttonTitle: Signal<String, NoError> { get }
   var hint: Signal<String, NoError> { get }
+  var image: Signal<UIImage?, NoError> { get }
+  var scoreLabel: Signal<String, NoError> { get }
 }
 
 public protocol ViewModelType {
@@ -19,15 +22,36 @@ public final class ViewModel: ViewModelType, ViewModelInputs, ViewModelOutputs {
 
   public init() {
 
-    self.doSomething = self.actionButtonTappedProperty.signal
-      .map { "Leave me alone!" }
+    let count = self.actionButtonTappedProperty.signal
+      .scan(0) { acc, _ in acc + 1 }
+
+    self.buttonTitle = count
+      .map { _ in "Don't press me" }
+
+    self.image = count
+      .map {
+        switch $0 {
+        case 0:
+          return nil
+        case 1...5:
+          return UIImage(named: "1")
+        case 5...10:
+          return UIImage(named: "2")
+        case 10...20:
+          return UIImage(named: "3")
+        default:
+          return UIImage(named: "4")
+        }
+    }
+
+    self.scoreLabel = count.map { String($0) }
 
     // a11y
     self.hint = self.actionButtonTappedProperty.signal
       .map { "Prints message." }
 
     // Tracking
-    self.doSomething
+    self.actionButtonTappedProperty.signal
       .observeNext { _ in AppEnvironment.current.panda.trackButtonTapped() }
   }
 
@@ -36,9 +60,20 @@ public final class ViewModel: ViewModelType, ViewModelInputs, ViewModelOutputs {
     self.actionButtonTappedProperty.value = ()
   }
 
-  public let doSomething: Signal<String, NoError>
+  private let viewDidLoadProperty = MutableProperty()
+  public func viewDidLoad() {
+    self.viewDidLoadProperty.value = ()
+  }
+
+  public let buttonTitle: Signal<String, NoError>
   public let hint: Signal<String, NoError>
+  public let image: Signal<UIImage?, NoError>
+  public let scoreLabel: Signal<String, NoError>
 
   public var inputs: ViewModelInputs { return self }
   public var outputs: ViewModelOutputs { return self }
+}
+
+private func isGameOver(score score: Int) -> Bool {
+  return score == 6
 }
